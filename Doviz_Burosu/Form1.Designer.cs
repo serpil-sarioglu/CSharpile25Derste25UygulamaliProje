15d6b5e334982fbc1a8d613f6116a86ea18dcdae
serpil-sarioglu <serpilsarioglu81@gmail.com>
2025-02-19 23:56:40 +0300

    Add project files

---
+++ Doviz_Burosu/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace Doviz_Burosu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEuroSatis = new System.Windows.Forms.Button();
+            this.btnEuroAlis = new System.Windows.Forms.Button();
+            this.btnDolarSatis = new System.Windows.Forms.Button();
+            this.btnDolarAlis = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDovizAl = new System.Windows.Forms.Button();
+            this.btnDovizSat = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtKalan = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtTutar = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtMiktar = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtKur = new System.Windows.Forms.TextBox();
+            this.lblEuroSatis = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblEuroAlis = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblDolarSatis = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblDolarAlis = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTL = new System.Windows.Forms.Label();
+            this.lblUSD = new System.Windows.Forms.Label();
+            this.lblEUR = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnEuroSatis
+            // 
+            this.btnEuroSatis.Location = new System.Drawing.Point(230, 198);
+            this.btnEuroSatis.Name = "btnEuroSatis";
+            this.btnEuroSatis.Size = new System.Drawing.Size(45, 35);
+            this.btnEuroSatis.TabIndex = 26;
+            this.btnEuroSatis.Text = "...";
+            this.btnEuroSatis.UseVisualStyleBackColor = true;
+            this.btnEuroSatis.Click += new System.EventHandler(this.btnEuroSatis_Click);
+            // 
+            // btnEuroAlis
+            // 
+            this.btnEuroAlis.Location = new System.Drawing.Point(230, 150);
+            this.btnEuroAlis.Name = "btnEuroAlis";
+            this.btnEuroAlis.Size = new System.Drawing.Size(45, 35);
+            this.btnEuroAlis.TabIndex = 25;
+            this.btnEuroAlis.Text = "...";
+            this.btnEuroAlis.UseVisualStyleBackColor = true;
+            this.btnEuroAlis.Click += new System.EventHandler(this.btnEuroAlis_Click);
+            // 
+            // btnDolarSatis
+            // 
+            this.btnDolarSatis.Location = new System.Drawing.Point(230, 102);
+            this.btnDolarSatis.Name = "btnDolarSatis";
+            this.btnDolarSatis.Size = new System.Drawing.Size(45, 35);
+            this.btnDolarSatis.TabIndex = 24;
+            this.btnDolarSatis.Text = "...";
+            this.btnDolarSatis.UseVisualStyleBackColor = true;
+            this.btnDolarSatis.Click += new System.EventHandler(this.btnDolarSatis_Click);
+            // 
+            // btnDolarAlis
+            // 
+            this.btnDolarAlis.Location = new System.Drawing.Point(230, 54);
+            this.btnDolarAlis.Name = "btnDolarAlis";
+            this.btnDolarAlis.Size = new System.Drawing.Size(45, 35);
+            this.btnDolarAlis.TabIndex = 23;
+            this.btnDolarAlis.Text = "...";
+            this.btnDolarAlis.UseVisualStyleBackColor = true;
+            this.btnDolarAlis.Click += new System.EventHandler(this.btnDolarAlis_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDovizAl);
+            this.groupBox1.Controls.Add(this.btnDovizSat);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.txtKalan);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.txtTutar);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.txtMiktar);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.txtKur);
+            this.groupBox1.Location = new System.Drawing.Point(314, 20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(359, 280);
+            this.groupBox1.TabIndex = 22;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Müşteri Döviz Al/Sat İşlemleri";
+            // 
+            // btnDovizAl
+            // 
+            this.btnDovizAl.Location = new System.Drawing.Point(226, 209);
+            this.btnDovizAl.Name = "btnDovizAl";
+            this.btnDovizAl.Size = new System.Drawing.Size(86, 40);
+            this.btnDovizAl.TabIndex = 14;
+            this.btnDovizAl.Text = "Döviz Al";
+            this.btnDovizAl.UseVisualStyleBackColor = true;
+            this.btnDovizAl.Click += new System.EventHandler(this.btnDovizAl_Click);
+            // 
+            // btnDovizSat
+            // 
+            this.btnDovizSat.Location = new System.Drawing.Point(116, 209);
+            this.btnDovizSat.Name = "btnDovizSat";
+            this.btnDovizSat.Size = new System.Drawing.Size(86, 40);
+            this.btnDovizSat.TabIndex = 9;
+            this.btnDovizSat.Text = "Döviz Sat";
+            this.btnDovizSat.UseVisualStyleBackColor = true;
+            this.btnDovizSat.Click += new System.EventHandler(this.btnDovizSat_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(32, 178);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(44, 16);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "Kalan:";
+            // 
+            // txtKalan
+            // 
+            this.txtKalan.Location = new System.Drawing.Point(107, 173);
+            this.txtKalan.Name = "txtKalan";
+            this.txtKalan.Size = new System.Drawing.Size(215, 22);
+            this.txtKalan.TabIndex = 12;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(32, 131);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(41, 16);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "Tutar:";
+            // 
+            // txtTutar
+            // 
+            this.txtTutar.Location = new System.Drawing.Point(107, 128);
+            this.txtTutar.Name = "txtTutar";
+            this.txtTutar.Size = new System.Drawing.Size(215, 22);
+            this.txtTutar.TabIndex = 10;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(25, 86);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(46, 16);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Miktar:";
+            // 
+            // txtMiktar
+            // 
+            this.txtMiktar.Location = new System.Drawing.Point(107, 83);
+            this.txtMiktar.Name = "txtMiktar";
+            this.txtMiktar.Size = new System.Drawing.Size(215, 22);
+            this.txtMiktar.TabIndex = 8;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(47, 41);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(29, 16);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Kur:";
+            // 
+            // txtKur
+            // 
+            this.txtKur.Location = new System.Drawing.Point(107, 38);
+            this.txtKur.Name = "txtKur";
+            this.txtKur.Size = new System.Drawing.Size(215, 22);
+            this.txtKur.TabIndex = 0;
+            // 
+            // lblEuroSatis
+            // 
+            this.lblEuroSatis.AutoSize = true;
+            this.lblEuroSatis.Location = new System.Drawing.Point(150, 205);
+            this.lblEuroSatis.Name = "lblEuroSatis";
+            this.lblEuroSatis.Size = new System.Drawing.Size(14, 16);
+            this.lblEuroSatis.TabIndex = 21;
+            this.lblEuroSatis.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(42, 205);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(71, 16);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Euro Satış:";
+            // 
+            // lblEuroAlis
+            // 
+            this.lblEuroAlis.AutoSize = true;
+            this.lblEuroAlis.Location = new System.Drawing.Point(150, 157);
+            this.lblEuroAlis.Name = "lblEuroAlis";
+            this.lblEuroAlis.Size = new System.Drawing.Size(14, 16);
+            this.lblEuroAlis.TabIndex = 19;
+            this.lblEuroAlis.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(54, 157);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 16);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Euro Alış:";
+            // 
+            // lblDolarSatis
+            // 
+            this.lblDolarSatis.AutoSize = true;
+            this.lblDolarSatis.Location = new System.Drawing.Point(150, 109);
+            this.lblDolarSatis.Name = "lblDolarSatis";
+            this.lblDolarSatis.Size = new System.Drawing.Size(14, 16);
+            this.lblDolarSatis.TabIndex = 17;
+            this.lblDolarSatis.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 16);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Dolar Satış:";
+            // 
+            // lblDolarAlis
+            // 
+            this.lblDolarAlis.AutoSize = true;
+            this.lblDolarAlis.Location = new System.Drawing.Point(150, 61);
+            this.lblDolarAlis.Name = "lblDolarAlis";
+            this.lblDolarAlis.Size = new System.Drawing.Size(14, 16);
+            this.lblDolarAlis.TabIndex = 15;
+            this.lblDolarAlis.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 16);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Dolar Alış:";
+            // 
+            // lblTL
+            // 
+            this.lblTL.AutoSize = true;
+            this.lblTL.Location = new System.Drawing.Point(720, 64);
+            this.lblTL.Name = "lblTL";
+            this.lblTL.Size = new System.Drawing.Size(14, 16);
+            this.lblTL.TabIndex = 27;
+            this.lblTL.Text = "0";
+            // 
+            // lblUSD
+            // 
+            this.lblUSD.AutoSize = true;
+            this.lblUSD.Location = new System.Drawing.Point(720, 121);
+            this.lblUSD.Name = "lblUSD";
+            this.lblUSD.Size = new System.Drawing.Size(14, 16);
+            this.lblUSD.TabIndex = 28;
+            this.lblUSD.Text = "0";
+            // 
+            // lblEUR
+            // 
+            this.lblEUR.AutoSize = true;
+            this.lblEUR.Location = new System.Drawing.Point(720, 169);
+            this.lblEUR.Name = "lblEUR";
+            this.lblEUR.Size = new System.Drawing.Size(14, 16);
+            this.lblEUR.TabIndex = 29;
+            this.lblEUR.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(893, 322);
+            this.Controls.Add(this.lblEUR);
+            this.Controls.Add(this.lblUSD);
+            this.Controls.Add(this.lblTL);
+            this.Controls.Add(this.btnEuroSatis);
+            this.Controls.Add(this.btnEuroAlis);
+            this.Controls.Add(this.btnDolarSatis);
+            this.Controls.Add(this.btnDolarAlis);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblEuroSatis);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblEuroAlis);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblDolarSatis);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblDolarAlis);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Döviz Bürosu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEuroSatis;
+        private System.Windows.Forms.Button btnEuroAlis;
+        private System.Windows.Forms.Button btnDolarSatis;
+        private System.Windows.Forms.Button btnDolarAlis;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnDovizAl;
+        private System.Windows.Forms.Button btnDovizSat;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtKalan;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtTutar;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtMiktar;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtKur;
+        private System.Windows.Forms.Label lblEuroSatis;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblEuroAlis;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblDolarSatis;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblDolarAlis;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTL;
+        private System.Windows.Forms.Label lblUSD;
+        private System.Windows.Forms.Label lblEUR;
+    }
+}
+
---
+++ Doviz_Burosu/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Doviz_Burosu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string baglantiYolu = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DovizBurosuDb;Integrated Security=True;";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string bugunKurlar = "https://www.tcmb.gov.tr/kurlar/today.xml";
+            var xmlDosya = new XmlDocument();
+            xmlDosya.Load(bugunKurlar);
+
+            string dolarAlis = xmlDosya.SelectSingleNode("Tarih_Date/Currency[@Kod='USD']/BanknoteBuying").InnerXml;
+            lblDolarAlis.Text = dolarAlis.Replace(".", ",");
+
+            string dolarSatis = xmlDosya.SelectSingleNode("Tarih_Date/Currency[@Kod='USD']/BanknoteSelling").InnerXml;
+            lblDolarSatis.Text = dolarSatis.Replace(".", ",");
+
+            string euroAlis = xmlDosya.SelectSingleNode("Tarih_Date/Currency[@Kod='EUR']/BanknoteBuying").InnerXml;
+            lblEuroAlis.Text = euroAlis.Replace(".", ",");
+
+            string euroSatis = xmlDosya.SelectSingleNode("Tarih_Date/Currency[@Kod='EUR']/BanknoteSelling").InnerXml;
+            lblEuroSatis.Text = euroSatis.Replace(".", ",");
+
+            KasayiGuncelle();
+
+        }
+
+        private void btnDolarAlis_Click(object sender, EventArgs e)
+        {
+            txtKur.Text = lblDolarAlis.Text;
+            btnDovizAl.Enabled = false;
+            btnDovizSat.Enabled = true; 
+        }
+
+        private void btnDolarSatis_Click(object sender, EventArgs e)
+        {
+            txtKur.Text = lblDolarSatis.Text;
+            btnDovizSat.Enabled = false;
+            btnDovizAl.Enabled = true;
+        }
+
+        private void btnEuroAlis_Click(object sender, EventArgs e)
+        {
+            txtKur.Text = lblEuroAlis.Text;
+            btnDovizAl.Enabled = false;
+            btnDovizSat.Enabled = true;
+        }
+
+        private void btnEuroSatis_Click(object sender, EventArgs e)
+        {
+            txtKur.Text = lblEuroSatis.Text;
+            btnDovizSat.Enabled = false;
+            btnDovizAl.Enabled = true;
+        }
+
+        // Müşteri döviz bozduruyor
+        private void btnDovizSat_Click(object sender, EventArgs e)
+        {
+            double kur, miktar, tutar;
+            kur = Convert.ToDouble(txtKur.Text);
+            miktar = Convert.ToDouble(txtMiktar.Text);
+
+            tutar = kur * miktar;
+            txtTutar.Text = tutar.ToString();
+
+            string dovizTuru = (txtKur.Text == lblDolarAlis.Text) ? "USD" : "EUR";
+            using (SqlConnection baglanti = new SqlConnection(baglantiYolu))
+            {
+                baglanti.Open();
+
+                string sorgu = @"
+                    UPDATE Cash SET Miktar = Miktar + @tutar WHERE ParaBirimi = 'TL';
+                    UPDATE Cash SET Miktar = Miktar - @miktar WHERE ParaBirimi = @dovizTuru;
+                ";
+
+                using (SqlCommand komut = new SqlCommand(sorgu,baglanti))
+                {
+                    komut.Parameters.AddWithValue("@tutar", tutar);
+                    komut.Parameters.AddWithValue("@miktar", miktar);
+                    komut.Parameters.AddWithValue("@dovizTuru", dovizTuru);
+                    komut.ExecuteNonQuery();
+                }
+            }
+            KasayiGuncelle();
+            MessageBox.Show("Döviz bozdurma işlemi tamamlandı.");            
+        }
+
+        // Müşteri elindeki parayla döviz alıyor
+        private void btnDovizAl_Click(object sender, EventArgs e)
+        {
+            double kur, verilenTL, alinanDoviz, kalan;
+            kur = Convert.ToDouble(txtKur.Text);
+            verilenTL = Convert.ToDouble(txtMiktar.Text); 
+
+            alinanDoviz = Convert.ToDouble(verilenTL / kur); 
+            txtTutar.Text = alinanDoviz.ToString();
+
+            kalan = verilenTL % kur;
+            txtKalan.Text = kalan.ToString();
+
+            string dovizTuru = (txtKur.Text == lblDolarSatis.Text) ? "USD" : "EUR";
+            using (SqlConnection baglanti = new SqlConnection(baglantiYolu))
+            {
+                baglanti.Open();
+
+                // Müşteri elindeki parayla döviz alırsa döviz bürosu kasasında döviz azalır, TL artar
+                string sorgu = @"
+                    UPDATE Cash SET Miktar = Miktar - @alinanDoviz WHERE ParaBirimi = @dovizTuru;
+                    UPDATE Cash SET Miktar = Miktar + (@verilenTL-@kalan) WHERE ParaBirimi = 'TL';
+                ";
+
+                using (SqlCommand komut = new SqlCommand(sorgu, baglanti))
+                {
+                    komut.Parameters.AddWithValue("@alinanDoviz", alinanDoviz);
+                    komut.Parameters.AddWithValue("@verilenTL", verilenTL);
+                    komut.Parameters.AddWithValue("@kalan", kalan);
+                    komut.Parameters.AddWithValue("@dovizTuru", dovizTuru);
+                    komut.ExecuteNonQuery();
+                }
+            }
+            KasayiGuncelle();
+            MessageBox.Show("Döviz alım işlemi tamamlandı.");            
+        }
+
+        private void KasayiGuncelle()
+        {
+            using (SqlConnection baglanti = new SqlConnection(baglantiYolu))
+            {
+                baglanti.Open();
+                string sorgu = "SELECT Id, ParaBirimi, Miktar FROM Cash";
+
+                using (SqlCommand komut = new SqlCommand(sorgu, baglanti))
+                {
+                    using (SqlDataReader dr = komut.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            string paraBirimi = dr["ParaBirimi"].ToString();
+                            double miktar = Convert.ToDouble(dr["Miktar"]);
+
+                            if (paraBirimi == "TL")
+                                lblTL.Text = miktar.ToString("N2") + " TL";
+
+                            if (paraBirimi == "USD")
+                                lblUSD.Text = miktar.ToString("N2") + " USD";
+
+                            if (paraBirimi == "EUR")
+                                lblEUR.Text = miktar.ToString("N2") + " EUR";
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
